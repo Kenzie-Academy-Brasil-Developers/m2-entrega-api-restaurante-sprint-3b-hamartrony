2751881db7c772f911965b93d8233e473ffd992a
Hamart Rony <hamart23@icloud.com>
2022-01-22 13:23:41 -0300

    adicionando heroku

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
-const app = require('./app')
+const app   = require('./app')
+const cors  = require('cors') 
 
+app.use(cors())
 const port = 3000
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Server is Run at Port ${port}`)
 })
